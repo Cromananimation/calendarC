321ef2997b893e1346e48f9aa45290f04b6aef47
Cesar A. Roman <130246064+Cromananimation@users.noreply.github.com>
2023-06-02 17:46:10 -0500

    fixing stash issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
 $(window).on("load", function () {
   function makeHours(nowTime){
     for(index = 0; index < 24; index++){
