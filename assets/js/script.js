3b608a3b3cf7ac7cd6c87ed08cc07b131abb2b45
Cesar A. Roman <130246064+Cromananimation@users.noreply.github.com>
2023-06-05 18:39:02 -0500

    fixed ixed structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 
 $(window).on("load", function () {
-
+  headerToday = dayjs().format("dddd, MMMM Do") 
+  $("#current-day").text(headerToday)
   setInterval(function() {
     var date = new Date();
     var h = date.getHours();
@@ -31,20 +32,20 @@ $(window).on("load", function () {
       else {
         whenFromNow = "present"
       }
-      let labelEl = $('<div id="hour-' + curHour + '"' + ' class="row time-block ' + whenFromNow + '"></div>')
-      let timeEl = $('<div class="col-2 col-md-1 hour text-center py-3" id="time-text-box">' + curHour + '</div>')
-      let textEl = $('<textarea class="col-8 col-md-10 description" rows="3"></textarea>')
-      let btnEl = $('<button class="btn saveBtn col-2 col-md-1" aria-label="save"></button>')
-      let iEl = $('<i class="fas fa-save" aria-hidden="true"></i>')
-      $(labelEl).append(timeEl)
-      $(labelEl).append(textEl)
-      $(labelEl).append(btnEl)
-      $(btnEl).append(iEl)
-      $(mainContainer).append(labelEl)
+      let labelZ = $('<div id="hour-' + curHour + '"' + ' class="row time-block ' + whenFromNow + '"></div>')
+      let timeZ = $('<div class="col-2 col-md-1 hour text-center py-3" id="time-text-box">' + curHour + '</div>')
+      let textZ = $('<textarea class="col-8 col-md-10 description" rows="3"></textarea>')
+      let btnZ = $('<button class="btn saveBtn col-2 col-md-1" aria-label="save"></button>')
+      let iZ = $('<i class="fas fa-save" aria-hidden="true"></i>')
+      $(labelZ).append(timeZ)
+      $(labelZ).append(textZ)
+      $(labelZ).append(btnZ)
+      $(btnZ).append(iZ) 
+
+      $(mainContainer).append(labelZ)
     }
     
   }
-  console.log("figgity")
   mainContainer = $("#time-container")
   timeRightNow = dayjs().hour()
   makeHours(timeRightNow)
@@ -52,20 +53,7 @@ $(window).on("load", function () {
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
+    // function? 
+
   });
   
